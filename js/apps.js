4c0d099f02762170f11a19480ead8e5ae8b08191
Judipie <hakesjude@gmail.com>
2026-05-19 09:52:26 -0400

    LOGIC DONE

--- js/apps.js
+++ js/apps.js
@@ -1,36 +1,94 @@
-let d = document.getElementById("dayInput");
-let m = document.getElementById("monthInput");
-let y = document.getElementById("yearInput");
+let day = document.getElementById("dayInput");
+let month = document.getElementById("monthInput");
+let year = document.getElementById("yearInput");
 const button = document.getElementById("button");
 let textInput = document.getElementsByTagName("input");
-const errorOutput = document.getElementsByTagName("h3");
+const errorOutput = document.getElementsByClassName("error");
 let errorOutputDay = document.getElementById("dayError");
 let errorOutputMonth = document.getElementById("monthError");
 let errorOutputYear = document.getElementById("yearError");
-errorOutput.style.display = 'none';
 
+// Hide errors on page reload
+for (let error of errorOutput) {
+    error.style.display = 'none';
+}
+
+// Get current date
 let calc = new Date();
 
+// Check for input
 for (let input of textInput) {
     input.addEventListener('input', () => {
-        if (d.value !== '' && d.value < 1) {
-            errorOutput.style.display = 'none';
-            d.value = 1;
-            errorOutputDay.style.display = 'block';
+        // clear old errors for new inputs
+        for (let error of errorOutput) {
+            error.style.display = 'none';
+            error.textContent = "";
+        }
+
+        // Reset border colors for new inputs
+        day.style.borderColor = '';
+        month.style.borderColor = '';
+        year.style.borderColor = '';
+
+        // Begin the nested loop!
+        if (day.value !== '') {
+            const dayNumber = Number(day.value); // ensures number isnt text
+            if (dayNumber < 1 || dayNumber > 31) { // Check to see if day is below range
+                day.value = '';
+                errorOutputDay.style.display = 'block';
+                errorOutputDay.textContent = 'Day must be between 1 and 31';
+                day.style.borderColor = 'var(--pri-red)';
+            } else {
+                if (month.value !== '') {
+                    const monthNumber = Number(month.value); // ensures number isnt text
+                    if (!Number.isFinite(monthNumber) || monthNumber < 1 || monthNumber > 12) { // checks to see if month is within range
+                        month.value = ''
+                        errorOutputMonth.style.display = 'block';
+                        errorOutputMonth.textContent = 'Month must be between 1 and 12';
+                        month.style.borderColor = 'var(--pri-red)';
+                    } else {
+                        const yearNumber = Number(year.value); // ensures number isnt text
+                        const daysInMonth = new Date(yearNumber, monthNumber, 0).getDate(); // checks for day list in each month and leap year (best. variable. ever.)
+                        if (dayNumber > daysInMonth) {
+                            day.value = '';
+                            errorOutputDay.style.display = 'block';
+                            errorOutputDay.textContent = `There are only ${daysInMonth} days in month ${monthNumber}`; // fancy shmancy
+                            day.style.borderColor = 'var(--pri-red)';
+                        } else {
+                            if (year.value !== '') {
+                                if (yearNumber > new Date().getFullYear()) { // check if inputted year is greater than the current year
+                                    year.value = '';
+                                    errorOutputYear.style.display = 'block';
+                                    errorOutputYear.textContent = 'Person must be alive today'
+                                    year.style.borderColor = 'var(--pri-red)';
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     });
 }
 
 button.addEventListener('click', () => {
-    let dateInput = new Date(y.value, m.value - 1, d.value);
+    // ensures numbers arent text
+    const yearNumber = Number(year.value);
+    const monthNumber = Number(month.value);
+    const dayNumber = Number(day.value);
+
+    // Creates a date from the inputs
+    let dateInput = new Date(yearNumber, monthNumber - 1, dayNumber);
+
     // Debug stuff
     console.log("Today's date is: " + calc.toLocaleDateString());
     console.log("The inputted date is: " + dateInput.toLocaleDateString());
 
-    let outputYear = calc.getFullYear() - y.value;
-    let outputMonth = calc.getMonth() - m.value - 1;
-    let outputDay = calc.getDate() - d.value;
+    let outputYear = calc.getFullYear() - yearNumber;
+    let outputMonth = calc.getMonth() - monthNumber - 1;
+    let outputDay = calc.getDate() - dayNumber;
 
+    // Handlers if calculation creates negative numbers
     if (outputDay < 0) {
         outputMonth--;
         const previousMonth = new Date(calc.getFullYear(), calc.getMonth(), 0);
@@ -42,6 +100,7 @@ button.addEventListener('click', () => {
         outputMonth += 12;
     }
 
+    // Complete output
     document.getElementById("yearOutput").innerHTML = outputYear;
     document.getElementById("monthOutput").innerHTML = outputMonth;
     document.getElementById("dayOutput").innerHTML = outputDay;
